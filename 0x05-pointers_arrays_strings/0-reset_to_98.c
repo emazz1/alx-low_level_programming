762d03d969e3a8a2ec153b3302effa746dd7f370
emazz1 <adebijaemmanuel@gmail.com>
2022-12-20 02:10:00 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
-#include <main.h>
+#include "main.h"
 /**
- * reset_to_98 - update the value of a car using pointers
- * @n: int to update
- * Retun: 0
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Return: 0 is success
+*/
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
